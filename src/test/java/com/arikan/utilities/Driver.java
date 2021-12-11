d320284cbc3e2c24b72e402ca6a965a42881f3c8
selcukarikan <selcukarikan1301@gmail.com>
2021-12-11 01:53:13 +0100

    WebDriver

---
+++ src/test/java/com/arikan/utilities/Driver.java
@@ -0,0 +1,24 @@
+package com.arikan.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+    static WebDriver driver;
+
+   public static WebDriver getDriver(){
+       if(driver==null){
+           WebDriverManager.chromedriver().setup();
+           driver=new ChromeDriver();
+
+       }
+       return driver;
+    }
+    public static void closeDriver(){
+       if (driver !=null){
+           driver.quit();
+           driver=null;
+       }
+    }
+}
