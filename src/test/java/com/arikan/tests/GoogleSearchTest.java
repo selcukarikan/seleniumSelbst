8839dcc54bf6994d1b04856d845416c6e21eb3fd
selcukarikan <selcukarikan1301@gmail.com>
2021-12-13 20:01:59 +0100

    ozet

--- src/test/java/com/arikan/tests/GoogleSearchTest.java
+++ src/test/java/com/arikan/tests/GoogleSearchTest.java
@@ -15,13 +15,27 @@ public class GoogleSearchTest {
         ReusableMethods.sleep(3000);
 
         //title getirme
-        String title= Driver.getDriver().getTitle();
-        System.out.println(title);
+        String title1= Driver.getDriver().getTitle();
+        System.out.println(title1);
+
+        //Navigate() methodu; sayfa uzerinden ileri veya geri gitme,refresh() sayfayi yeniler
+        // back() bir onceki sayfaya getirir,forward() geri geldigimiz sayfaya getirir, to() istedigmiz sayfaya yonlendirir
+        //navigate de gecmisiz ve cerezlerimiz vardir---get de ise ilk defa gittigimiz de kullanilir
+        Driver.getDriver().navigate().to("https://www.amazon.com");
+        String title2= Driver.getDriver().getTitle();
+        System.out.println(title2);
+
+        //Google a geri donelim
+    //    Driver.getDriver().navigate().back();
+        String title3= Driver.getDriver().getTitle();
+        System.out.println(title3);
+
+
 
         //quit() acik olan tum pencereleri kapatir
         //close() sadece acik olan sayfayi kapatir
-        Driver.getDriver().close();
-        Driver.getDriver().quit();
+       // Driver.getDriver().close();
+        // Driver.getDriver().quit();
 
     }
 
