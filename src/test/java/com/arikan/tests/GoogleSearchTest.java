77904a0bd5ab102680b63866d5db0c92a93a2f1b
selcukarikan <selcukarikan1301@gmail.com>
2021-12-14 10:35:37 +0100

    partialLinkText

--- src/test/java/com/arikan/tests/GoogleSearchTest.java
+++ src/test/java/com/arikan/tests/GoogleSearchTest.java
@@ -2,7 +2,11 @@ package com.arikan.tests;
 
 import com.arikan.utilities.Driver;
 import com.arikan.utilities.ReusableMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class GoogleSearchTest {
 
@@ -36,6 +40,24 @@ public class GoogleSearchTest {
         //close() sadece acik olan sayfayi kapatir
        // Driver.getDriver().close();
         // Driver.getDriver().quit();
+
+      //  WebElement searchBox=Driver.getDriver().findElement(By.id("twotabsearchtextbox"));
+        // searchBox.sendKeys("kopfhörer kabellos");
+
+        Driver.getDriver().navigate().to("https://www.amazon.com/dp/B07WC141LZ/ref=sspa_dk_detail_4?psc=1&pd_rd_i=B07WC141LZ&pd_rd_w=4SPjS&pf_rd_p=54ed5474-54a8-4c7f-a88a-45f748d18166&pd_rd_wg=BTvLE&pf_rd_r=JXHRPTESBFQBC6PYGZC2&pd_rd_r=21e9e5e6-21be-4e9b-841a-a278ccfc0ee1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyNkZUUzg5NFpGMFQ2JmVuY3J5cHRlZElkPUEwMzM0NzM1MUtETVc5TzJZUFRSWSZlbmNyeXB0ZWRBZElkPUEwMjU0MDYyMUdDWlRCTU9NQjVVTCZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=");
+      WebElement title=Driver.getDriver().findElement(By.tagName("h1"));
+        System.out.println(title.getText());
+
+
+         WebElement beantwortFrage=Driver.getDriver().findElement(By.partialLinkText("95 beantwortete Fragen"));
+        System.out.println(beantwortFrage.getText());
+
+        List<WebElement> links=Driver.getDriver().findElements(By.tagName("a"));
+        for (WebElement element:links
+             ) {
+            System.out.println(element.getText());
+
+        }
 
     }
 
